63531abe5a2a6f07eff3b422b717161124fd5d04
happydilraj <happydilraj1022@gmail.com>
2023-04-04 13:56:21 +0530

    4 april 2023

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 function update() {
     fetch("/data.json")           //api for the get request
         .then(response => response.json())
         .then(res => {
-            console.log(res)
+            // console.log(res)
 
 
             res.data.forEach(element => {
@@ -11,6 +12,7 @@ function update() {
                 longitude = element.longitude;
                 latitude = element.latitude;
                 cases = element.infected;
+                country = element.name;
 
                 if(cases >= 255)
                 color = "rgb(255,0,0)";
@@ -18,12 +20,21 @@ function update() {
                 else
                 color = `rgb($cases,0,0)` 
 
+                const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+                    `Corona Cases in ${country} are ${cases}`
+                  );
+
 
                 const marker1 = new mapboxgl.Marker({
                     draggable: false,
                     color : color
                 })
+                // create DOM element for the marker
+                const el = document.createElement("div");
+                el.id = "marker";
+                new mapboxgl.Marker(el)
                     .setLngLat([longitude, latitude])
+                    .setPopup(popup)
                     .addTo(map)
                  });
                 map.addControl(new mapboxgl.FullscreenControl());
